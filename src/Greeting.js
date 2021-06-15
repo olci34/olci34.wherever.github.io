cf7fb8d08cf5da6ed6a5009bb4572d389b8db1ae
github username <github email address>
2021-06-15 13:25:52 -0400

    edits  Greeting Component text

--- src/Greeting.js
+++ src/Greeting.js
@@ -6,8 +6,7 @@ export default function Greeting() {
     <div id="greeting">
       <h1 id='welcome-header'>Welcome to Wherever Space Cruise</h1>
       <p>Wherever you are in Solar System and</p>
-      <p>wherever you want to go to in Solar System,</p>
-      <p>We got you boo !</p>
+      <p>wherever you want to go to in Solar System</p>
       <UserForm serve="Sign In" />
       <br/>
       <Link to="/signup">Sign Up</Link>
