e0cdf1ad473f499a7f91ebca8b094aad7276ce69
olci34 <developerogulcan@gmail.com>
2023-04-21 18:30:38 -0400

    fix typo

--- src/Greeting.js
+++ src/Greeting.js
@@ -3,7 +3,7 @@ import GreetingOptions from "./users/GreetingOptions";
 export default function Greeting() {
   return (
     <div id="greeting">
-      <h1 id='welcome-header'>Welcome to Wherever Space Cruise Apps</h1>
+      <h1 id='welcome-header'>Welcome to Wherever Space Cruise</h1>
       <p>Sign in or sign up to book a trip.</p>
       <p>Learn more about the planets without signing up.</p>
       <GreetingOptions/>
